c1e14276efc0697aabd07ef9710bfacf3ca02d02
natashaterekhova <nate1813@colorado.edu>
2024-11-05 10:42:14 -0700

    updated ica 11

--- ica/ica11.js
+++ ica/ica11.js
@@ -2,14 +2,11 @@ function tellFortune(numChild, partnerName, location, job){
     return("You will be a " + job + " in " + location + " and married to " + partnerName + " with " + numChild + " kids.");
 }
 
-console.log(tellFortune("3", "Steve", "Portugal", "designer"));
-console.log(tellFortune("18", "Caroline", "Denver", "electrician"));
-console.log(tellFortune("0", "Albert", "Virginia", "coder"));
+document.getElementById("my_para").innerHTML = tellFortune("3", "Steve", "Portugal", "designer") + tellFortune("18", "Caroline", "Denver", "electrician") + tellFortune("0", "Albert", "Virginia", "coder") + calculateDogAge(7);
 
 function calculateDogAge(puppyage){
     dogYear = puppyage*7;
     return("Your dog is " + dogYear + " years old in dog years!");
 }
 
-console.log(calculateDogAge(7));
 
